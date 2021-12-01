fa48b6483bb3abe8a413900ae6782921ad6a42c3
Benoit FREMON <bfremon@gmail.com>
2021-12-01 22:00:53 +0100

    stkup_dim.rnd_scal(), stkup.monte_carlo(): new funcs stkup_dim.compare(): min, max replaced by mu, s

--- cmp_stkup.py
+++ cmp_stkup.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
 
 import numpy as np
+from scipy import stats
+import pandas as pd
+import matplotlib.pyplot as plt
+from pl0t import *
 
 lsl_a = 5
 usl_a = 10
@@ -12,17 +16,20 @@ usl_c = 16
 class stkup_dim():
 
     
-    def __init__(self, name, direction, lsl, usl,
-                 mu_hat=None, std_hat=None, Ppk_min=None):
+    def __init__(self, name, direction, lsl, usl, dist='norm',
+                 mu_hat=None, std_hat=None, Ppk_min=None,
+                 rnd_size = 10**6):
         '''
         Create a stackup dimension object
         name: dimension name
         direction: dimension direction / coefficient
         lsl: Lower Spec Limit
         usl: Upper Spec Limit
+        dist: either normal, uniform or generalized normal type 1 statistical distributions.
         mu_hat: estimated mean
         std_hat: estimated standard deviation
         Ppk_min: minimum process capability
+        rndm_size: len() of random values
         '''
 
         self.name = name
@@ -32,8 +39,9 @@ class stkup_dim():
         self.__must_be_sup(lsl, usl)
         self.lsl = lsl
         self.usl = usl
+        self.dist = dist
         self.nominal = self.__calc_nom()
-        self.mean_hat = None
+        self.mu_hat = None
         self.std_hat = None
         self.Ppk_min = None
         if std_hat:
@@ -45,7 +53,10 @@ class stkup_dim():
                 self.__err_ppk_std()
             self.Ppk_min = Ppk_min
             self.std_hat = self.__calc_std()
-
+        if not mu_hat:
+            self.mu_hat = self.__calc_nom()
+        else:
+            self.mu_hat = mu_hat
             
     def __must_be_sup(self, mini, maxi):
         if mini >= maxi:
@@ -57,11 +68,18 @@ class stkup_dim():
 
 
     def __calc_std(self):
-        if self.mean_hat:
+        if self.mu_hat == self.__calc_nom():
             print('off center mean not supported')
             pass
         ret = abs(self.usl - self.nominal) / (3 * self.Ppk_min)
         return ret        
+
+    
+    def rndm_scal(self):
+        if not self.mu_hat and not self.std_hat:
+            raise SyntaxError('mean and std must be specified to draw random number')
+        ret = stats.norm.rvs(loc=self.mu_hat, scale=self.std_hat)
+        return ret
 
     
     def __err_ppk_std(self):
@@ -85,6 +103,23 @@ class stkup():
         self.nominal = self.nominal()
 
         
+    def get_inputs(self):
+        ret = {'name': [], 'dist': [], 'direction': [],'lsl': [], 'usl': [],
+               'nominal': [], 'mu_hat': [], 'std_hat': [],
+               'Ppk_min': []}
+        for dim in self.dims:
+            ret['name'].append(str(dim.name))
+            ret['direction'].append(str(dim.direction))
+            ret['dist'].append(str(dim.dist))
+            ret['lsl'].append(str(dim.lsl))
+            ret['usl'].append(str(dim.usl))
+            ret['nominal'].append(str(dim.nominal))
+            ret['mu_hat'].append(str(dim.mu_hat))
+            ret['std_hat'].append(str(dim.std_hat))
+            ret['Ppk_min'].append(str(dim.Ppk_min))
+        return pd.DataFrame(ret).transpose()
+    
+                        
     def compare(self):
         '''
         Compare different stackup options
@@ -93,12 +128,17 @@ class stkup():
         print('Nominal: %1.3f' % self.nominal)
         print('Worst case - min: %1.3f, max: %1.3f' % self.worst_case())
         tol_ti = self.stats('ti')
-        print('Statistical (tolerance interval) - min %1.3f, max %1.3f' %
-              (self.nominal - tol_ti, self.nominal + tol_ti))
+        print('Statistical (tolerance interval) - mu %1.3f, s %1.3f' %
+              (self.nominal, tol_ti))
         tol_ppk = self.stats('std')
-        print('Statistical (std) - min %1.3f, max %1.3f' %
-              (self.nominal - tol_ppk, self.nominal + tol_ppk))
-
+        print('Statistical (std) - mu %1.3f, s %1.3f' %
+              (self.nominal, tol_ppk))
+        mc_pop = self.monte_carlo()
+        mc_pop_std = np.std(mc_pop)
+        mc_pop_mu = np.mean(mc_pop)
+        print('Monte Carlo - mu %1.3f, std %1.3f' %
+              (mc_pop_mu, mc_pop_std))
+        
               
     def worst_case(self):
         '''
@@ -126,25 +166,36 @@ class stkup():
         return ret
 
 
-    def stats(self, type='std'):
+    def stats(self, stk='std'):
         '''
         return statistical tolerance interval
-        type: either-
+        stk: either-
              - ti - tolerance interval,
              - std - observed standard deviation, 
         '''
         ret = 0
         for dim in self.dims:
-            if type == 'ti':
+            if stk == 'ti':
                 ti = (dim.usl - dim.lsl) / 2
                 ret += ti ** 2
-            elif type == 'std':
+            elif stk == 'std':
                 ret += dim.std_hat ** 2
             else:
-                raise SyntaxError('Unknown statistical stackup: %s' % str(type))
+                raise SyntaxError('Unknown statistical stackup: %s' % str(stk))
         return np.sqrt(ret)
 
     
+    def monte_carlo(self, draws = 10**4):
+        cnt = 0
+        ret = []
+        for i in range(draws):
+            stk = 0
+            for dim in self.dims:
+                stk += dim.direction * dim.rndm_scal()
+            ret.append(stk)
+        return ret
+    
+                    
     def __set_name_coef(self, dim, i):
         ret = ''
         if dim.direction < -1:
@@ -163,8 +214,11 @@ class stkup():
                 ret = str(dim.direction) + '*' 
         return ret 
 
+    
 if __name__ == '__main__':
     dim_a = stkup_dim('a', 1,  5, 10, Ppk_min=1.33)
     dim_b = stkup_dim('b', 1, 6, 12, Ppk_min=1.0)
     dim_c = stkup_dim('c', -1, 9, 11, Ppk_min=1.5)
-    stkup(dim_a, dim_b, dim_c).compare()
+    stk = stkup(dim_a, dim_b, dim_c)
+    stk.compare()
+    print(stk.get_inputs())
